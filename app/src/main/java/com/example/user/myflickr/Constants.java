3c29ca29c3cdbe4aea01b04cf13d60fefd5ebc59
Krishnakumar Rajendran <krishnasinbox@outlook.com>
2015-05-08 01:31:55 -0700

    first commit
    MyFlickr Part1

---
+++ app/src/main/java/com/example/user/myflickr/Constants.java
@@ -0,0 +1,17 @@
+package com.example.user.myflickr;
+
+public class Constants {
+    public static final int NUM_PHOTOS = 10;
+    public static final String TAG = "my code";
+    public static final String API_KEY = "364a39c0c5d3aa4dffc36a905525b27b";
+    public static final String SECRET = "03ec30d2884d0c12";
+    //    public static final String LATITUDE = "34.0672280";
+//    public static final String LONGITUDE = "-118.1667410";
+    public static final String RADIUS = "1";
+
+    public static final String LATITUDE = "40.7127837";
+    public static final String LONGITUDE = "-74.00594130000002";
+
+
+
+}
